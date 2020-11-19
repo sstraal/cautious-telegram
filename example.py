4d8d86cfdfb7363a48b7df061321465e7312de37
sstraal <samayrastraal@gmail.com>
2020-11-19 10:52:16 +0100

    Uncommented the failing function

--- example.py
+++ example.py
@@ -1,6 +1,13 @@
+import numpy as np
+
 def add(a,b):
     return a + b
 
 def test_add():
     assert add(2,3) == 5
     assert add("space", "ship") == "spaceship"
+
+# uncomment the following test in step 5
+def test_subtract():
+    assert np.subtract(2, 3) == -1
+
